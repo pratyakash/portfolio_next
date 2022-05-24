8cea663e495374f74ce383ecc2c8d15cbb9e7123
pratyakash <pratyakash13@gmail.com>
2022-05-24 23:24:23 +0530

    Changing Index

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ export default function Home() {
 		<Head>
 			<title>Pratyakash Saini</title>
 			<meta name="description" content="Pratyakash Saini" />
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
